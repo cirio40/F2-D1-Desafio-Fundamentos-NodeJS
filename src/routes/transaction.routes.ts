8549558b01a1cd0d08355f1c91b2e6039206273a
Círio José Canheski <canheski@outlook.com>
2020-04-17 18:06:05 -0300

    Conclusão do desafio 1 Fase 2 Fundamentos NodeJS

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+const { uuid } = require ("uuidv4");
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+
+const repositories = [];
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -17,7 +20,17 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+    repositories.push(transaction);
+    return response.json(transaction);
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
